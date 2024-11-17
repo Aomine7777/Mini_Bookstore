fea949abe602f96772e1576a4686997376ae58b7
Aomine7777 <141518740+Aomine7777@users.noreply.github.com>
2024-11-17 12:13:00 +0200

    Initial commit

---
+++ src/main/java/com/example/Mini_Bookstore/MiniBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mini_Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Mini_Bookstore/MiniBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mini_Bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
